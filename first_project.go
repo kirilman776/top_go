60194a85c01870529a0ad0533bd0215948eb9dfa
kirilman776 <135834386+kirilman776@users.noreply.github.com>
2024-09-11 13:49:14 +0300

    Update first_project.go

--- first_project.go
+++ first_project.go
@@ -1,7 +1,71 @@
 package main
 
-import "fmt"
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
+    "strings"
+)
+
+
+func add(a, b int) int  {
+    return a + b
+}
+
+func subtract(a, b int) int {
+    return a - b
+}
+
+func multiply(a, b int) int {
+    return a * b
+}
+
+func divide(a, b int) int {
+    if b == 0 {
+        fmt.Println("Ошибка: деление на ноль!")
+        return 0
+    }
+    return a / b
+}
+
+
 
 func main() {
-	fmt.Println("Hello Kata Academy")
+    var oper string
+    var newtext []string
+    reader := bufio.NewReader(os.Stdin)
+    
+    
+    fmt.Println("Введите пример:")
+    text, _ := reader.ReadString('\n')
+    operators := [4]string{"+","-", "/", "*"}
+    for _, operator := range operators {
+      if strings.Contains(text, operator){
+        newtext = strings.Split(strings.TrimSpace(text), operator)
+        oper = operator
+      }
+    }
+    fmt.Println(newtext)
+    a, _ := strconv.Atoi(newtext[0])
+    b, _ := strconv.Atoi(newtext[1])
+    fmt.Println(a,b,oper)
+    
+    var result int
+
+    switch oper {
+    case "+":
+        result = add(a, b)
+    case "-":
+        result = subtract(a, b)
+    case "*":
+        result = multiply(a, b)
+    case "/":
+        result = divide(a, b)
+    default:
+        fmt.Println("Неизвестный оператор")
+        return
+    }
+
+    fmt.Printf("Результат:", result)
 }
