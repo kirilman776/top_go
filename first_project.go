414dc50b4b3fcdcc649649845dc703d96bbcfc4c
kirilman776 <135834386+kirilman776@users.noreply.github.com>
2024-09-22 13:35:32 +0300

    Calculator.go

--- first_project.go
+++ first_project.go
@@ -1,71 +1,74 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "strconv"
-    "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
+func readInteger() float64 {
+	var number float64
+	_, err := fmt.Scanln(&number)
+	if err != nil {
 
-func add(a, b int) int  {
-    return a + b
+		fmt.Println(fmt.Sprintf("Не получилось прочитать число: %s", err))
+	}
+	return number
 }
 
-func subtract(a, b int) int {
-    return a - b
+func readOper() string {
+	reader := bufio.NewReader(os.Stdin)
+	oper, _ := reader.ReadString('\n')
+	oper = strings.Trim(oper, "\n")
+	return oper
 }
 
-func multiply(a, b int) int {
-    return a * b
+func add(a, b float64) (float64, string) {
+	return a + b, "сложения"
 }
 
-func divide(a, b int) int {
-    if b == 0 {
-        fmt.Println("Ошибка: деление на ноль!")
-        return 0
-    }
-    return a / b
+func subtract(a, b float64) (float64, string) {
+	return a - b, "вычитания"
 }
 
+func multiply(a, b float64) (float64, string) {
+	return a * b, "умножения"
+}
 
+func divide(a, b float64) (float64, string) {
+	if b == 0 {
+		fmt.Println("Ошибка: деление на ноль!")
+		return 0, ""
+	}
+	return a / b, "деления"
+}
 
 func main() {
-    var oper string
-    var newtext []string
-    reader := bufio.NewReader(os.Stdin)
-    
-    
-    fmt.Println("Введите пример:")
-    text, _ := reader.ReadString('\n')
-    operators := [4]string{"+","-", "/", "*"}
-    for _, operator := range operators {
-      if strings.Contains(text, operator){
-        newtext = strings.Split(strings.TrimSpace(text), operator)
-        oper = operator
-      }
-    }
-    fmt.Println(newtext)
-    a, _ := strconv.Atoi(newtext[0])
-    b, _ := strconv.Atoi(newtext[1])
-    fmt.Println(a,b,oper)
-    
-    var result int
+	var result float64
+	var action string
 
-    switch oper {
-    case "+":
-        result = add(a, b)
-    case "-":
-        result = subtract(a, b)
-    case "*":
-        result = multiply(a, b)
-    case "/":
-        result = divide(a, b)
-    default:
-        fmt.Println("Неизвестный оператор")
-        return
-    }
+	a := readInteger()
+	oper := readOper()
+	b := readInteger()
 
-    fmt.Printf("Результат:", result)
+	switch oper {
+	case "+":
+		result, action = add(a, b)
+	case "-":
+		result, action = subtract(a, b)
+	case "*":
+		result, action = multiply(a, b)
+	case "/":
+		result, action = divide(a, b)
+		if result == 0 {
+			return
+		}
+	default:
+		fmt.Println("Неизвестный оператор")
+		return
+	}
+
+	fmt.Printf("Результат %s: %.2f", action, result)
+	fmt.Println()
 }
